56021cfebb0e50025519bc7d6081c014373bcae7
wenlai <wenchenglai@gmail.com>
2014-09-29 20:40:01 -0400

    added madison

---
+++ CIPMSWeb/Enrollment/Madison/Step2_2.aspx.cs
@@ -0,0 +1,356 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Web.UI.WebControls;
+using CIPMSBC;
+using CIPMSBC.Eligibility;
+
+public partial class Step2_Chi_2 : System.Web.UI.Page
+{
+    private CamperApplication CamperAppl;
+    private General objGeneral;
+    private Boolean bPerformUpdate;
+
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        btnNext.Click += new EventHandler(btnNext_Click);
+        btnPrevious.Click += new EventHandler(btnPrevious_Click);
+        btnSaveandExit.Click += new EventHandler(btnSaveandExit_Click);
+        btnReturnAdmin.Click += new EventHandler(btnReturnAdmin_Click);
+        CusValComments.ServerValidate += new ServerValidateEventHandler(CusValComments_ServerValidate);
+        CusValComments1.ServerValidate += new ServerValidateEventHandler(CusValComments_ServerValidate);
+    }
+
+    void btnReturnAdmin_Click(object sender, EventArgs e)
+    {
+        string strRedirURL;
+        try
+        {
+            if (Page.IsValid)
+            {
+                strRedirURL = ConfigurationManager.AppSettings["AdminRedirURL"].ToString();
+                ProcessCamperAnswers();
+                Response.Redirect(strRedirURL);
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            CamperAppl = new CamperApplication();
+            objGeneral = new General();
+            if (!Page.IsPostBack)
+            {
+                //to fill the grades in the dropdown
+                getGrades();
+                //to get the FJCID which is stored in session
+                if (Session["FJCID"] != null)
+                {
+                    hdnFJCIDStep2_2.Value = (string)Session["FJCID"];
+                    PopulateAnswers();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+    }
+
+   void btnSaveandExit_Click(object sender, EventArgs e)
+    {
+        string strRedirURL;
+        try
+        {
+            if (Page.IsValid)
+            {
+                strRedirURL = Master.SaveandExitURL;
+                if (!objGeneral.IsApplicationReadOnly(hdnFJCIDStep2_2.Value, Master.CamperUserId))
+                {
+                    ProcessCamperAnswers();
+                }
+                if (Master.IsCamperUser == "Yes")
+                {
+
+                    var oGen = new General();
+                    if (oGen.IsApplicationSubmitted(Session["FJCID"].ToString()))
+                    {
+                        Response.Redirect(strRedirURL);
+                    }
+                    else
+                    {
+                        string strScript = "<script language=javascript>openThis(); window.location='" + strRedirURL + "';</script>";
+                        if (!ClientScript.IsStartupScriptRegistered("clientScript"))
+                        {
+                            ClientScript.RegisterStartupScript(Page.GetType(), "clientScript", strScript);
+                        }
+                    }
+                }
+                else
+                {
+                    Response.Redirect(strRedirURL);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+    }
+
+    void btnPrevious_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (Page.IsValid)
+            {
+                if (!objGeneral.IsApplicationReadOnly(hdnFJCIDStep2_2.Value, Master.CamperUserId))
+                {
+                    ProcessCamperAnswers();
+                }
+                Session["FJCID"] = hdnFJCIDStep2_2.Value;
+                    Response.Redirect("Summary.aspx");
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+    }
+
+    void btnNext_Click(object sender, EventArgs e)
+    {
+        int iStatus;
+        string strModifiedBy, strFJCID;
+        EligibilityBase objEligibility = EligibilityFactory.GetEligibility(FederationEnum.CNY);
+        
+        try
+        {
+            if (Page.IsValid)
+            {
+                if (!objGeneral.IsApplicationReadOnly(hdnFJCIDStep2_2.Value, Master.CamperUserId))
+                {
+                    ProcessCamperAnswers();
+                }
+                bool isReadOnly = objGeneral.IsApplicationReadOnly(hdnFJCIDStep2_2.Value, Master.CamperUserId);
+                //Modified by id taken from the Master Id
+                strModifiedBy = Master.UserId;
+                strFJCID = hdnFJCIDStep2_2.Value;
+                if (strFJCID != "" && strModifiedBy != "")
+                {
+                    if (isReadOnly)
+                    {
+                        DataSet dsApp = CamperAppl.getCamperApplication(strFJCID);
+                        iStatus = Convert.ToInt16(dsApp.Tables[0].Rows[0]["Status"]);
+                    }
+                    else
+                    {
+
+                        //to check whether the camper is eligible 
+                        objEligibility.checkEligibilityforStep2(strFJCID, out iStatus);
+                    }
+
+                    Session["STATUS"] = iStatus.ToString();
+                }
+                Session["FJCID"] = hdnFJCIDStep2_2.Value;
+                Response.Redirect("Step2_3.aspx");
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+    }
+
+    private void ProcessCamperAnswers()
+    {
+        string strFJCID;
+        string strComments;
+        int iGrade, iResult;
+        int iRowsAffected;
+        string strModifiedBy, strGrade;
+
+        //Modified by id taken from the common.master
+        strModifiedBy = Master.UserId;
+
+        InsertCamperAnswers();
+
+        //to update the grade to the database - to be used by the search
+        strFJCID = hdnFJCIDStep2_2.Value;
+        //used only by the Admin user
+        strComments = txtComments.Text.Trim();
+        if (strFJCID != "" && strModifiedBy != "" && bPerformUpdate)
+        {
+            strGrade = ddlGrade.SelectedValue;
+            int.TryParse(strGrade, out iResult);
+            if (iResult == 0 || strGrade.Equals(string.Empty))
+                iGrade = 0;
+            else
+                iGrade = iResult;
+
+            iRowsAffected = CamperAppl.updateGrade(strFJCID, iGrade, strComments, Convert.ToInt16(strModifiedBy));
+        }
+    }
+
+    protected void InsertCamperAnswers()
+    {
+        string strFJCID, strComments;
+        int RowsAffected;
+        string strModifiedBy, strCamperAnswers; //-1 for Camper (User id will be passed for Admin user)
+
+        strFJCID = hdnFJCIDStep2_2.Value;
+
+        //to get the comments text (used only by the Admin user)
+        strComments = txtComments.Text.Trim();
+
+        //Modified by id taken from the common.master
+        strModifiedBy = Master.UserId;
+
+        strCamperAnswers = ConstructCamperAnswers();
+
+        if (strFJCID != "" && strCamperAnswers != "" && strModifiedBy != "" && bPerformUpdate)
+            RowsAffected = CamperAppl.InsertCamperAnswers(strFJCID, strCamperAnswers, strModifiedBy, strComments);
+        CamperAppl = new CamperApplication();
+       CamperAppl.UpdateTimeInCampInApplication(strFJCID);
+    }
+
+    private void getGrades()
+    {
+        DataTable dtGrade;
+        dtGrade = objGeneral.getGrades(Session["FedId"].ToString(), Master.CampYear);
+        ddlGrade.DataSource = dtGrade;
+        ddlGrade.DataTextField = "EligibleGrade";
+        ddlGrade.DataValueField = "EligibleGrade";
+        ddlGrade.DataBind();
+        ddlGrade.Items.Insert(0, new ListItem("-- Select --", "0"));
+    }
+
+    void PopulateAnswers()
+    {
+        DataSet dsAnswers = CamperAppl.getCamperAnswers(hdnFJCIDStep2_2.Value, "", "", "3,6,7,8,1063");
+
+        foreach (DataRow dr in dsAnswers.Tables[0].Rows)
+        {
+            var qID = (QuestionId)dr["QuestionId"];
+
+            if (qID == QuestionId.FirstTime) // Is this your first time to attend a Non-profit Jewish overnight camp, for 3 weeks or longer:
+            {
+                if (dr["OptionID"].Equals(DBNull.Value))
+                    continue;
+
+                if (dr["OptionID"].ToString() == "1")
+                {
+                    rdoFirstTimerYes.Checked = true;
+                }
+                else
+                {
+                    rdoFirstTimerNo.Checked = true;
+                }
+            }
+            else if (qID == QuestionId.Grade) // Grade (Mention the grade of the camper after he/she attends the camp session):
+            {
+                if (!dr["Answer"].Equals(DBNull.Value))
+                {
+                    ddlGrade.SelectedValue = dr["Answer"].ToString();
+                }
+            }
+            else if (qID == QuestionId.SchoolType) // What kind of the school the camper go to
+            {
+                if (dr["OptionID"].Equals(DBNull.Value))
+                    continue;
+
+                rdoSchoolType.SelectedValue = dr["OptionID"].ToString();
+                if (dr["OptionID"].ToString() == "3")
+                    txtSchoolName.Enabled = false;
+            }
+            else if (qID == QuestionId.SchoolName) // Name of the school Camper attends:
+            {
+                if (!dr["Answer"].Equals(DBNull.Value))
+                {
+                    txtSchoolName.Text = dr["Answer"].ToString();
+                }
+            }
+            else if (qID == QuestionId.GrandfatherPolicySessionLength) // If a professional or fellow congregant is selected, offer this list as a check all that apply
+            {
+                if (dr["OptionID"].Equals(DBNull.Value))
+                    continue;
+
+                if (dr["OptionID"].ToString() == "1")
+                    rdoDays12.Checked = true;
+                else
+                    rdoDays19.Checked = true;
+            }
+
+
+        }
+    }
+
+    private string ConstructCamperAnswers()
+    {
+        var strQId = "";
+        string strTablevalues = "";
+
+        //to get the Question separator from Web.config
+        var strQSeparator = ConfigurationManager.AppSettings["QuestionSeparator"];
+        //to get the Field separator from Web.config
+        var strFSeparator = ConfigurationManager.AppSettings["FieldSeparator"];
+
+        //for question FirstTimerOrNot
+        strQId = ((int)QuestionId.FirstTime).ToString();
+        strTablevalues += strQId + strFSeparator + Convert.ToString(rdoFirstTimerYes.Checked ? "1" : rdoFirstTimerNo.Checked ? "2" : "") + strFSeparator + strQSeparator;
+
+        //Grandfaother question
+        strQId = ((int)QuestionId.GrandfatherPolicySessionLength).ToString();
+        strTablevalues += strQId + strFSeparator + (rdoDays12.Checked ? "1" : rdoDays19.Checked ? "2" : "") + strFSeparator + strQSeparator;
+
+        //for question Grade
+        strQId = ((int)QuestionId.Grade).ToString();
+        strTablevalues += strQId + strFSeparator + strFSeparator + ddlGrade.SelectedValue + strQSeparator;
+
+        //for question School Type
+        if (rdoSchoolType.SelectedValue != "")
+        {
+            strQId = ((int)QuestionId.SchoolType).ToString();
+            strTablevalues += strQId + strFSeparator + rdoSchoolType.SelectedValue + strFSeparator + rdoSchoolType.SelectedItem.Text + strQSeparator;
+        }
+
+        //for question School Name
+        strQId = ((int)QuestionId.SchoolName).ToString();
+        strTablevalues += strQId + strFSeparator + strFSeparator + txtSchoolName.Text + strQSeparator;
+
+        //to remove the extra character at the end of the string, if any
+        char[] chartoRemove = { Convert.ToChar(strQSeparator) };
+        strTablevalues = strTablevalues.TrimEnd(chartoRemove);
+
+        return strTablevalues;
+    }
+
+    void CusValComments_ServerValidate(object source, ServerValidateEventArgs args)
+    {
+        try
+        {
+            string strCamperAnswers, strUserId, strCamperUserId, strComments, strFJCID;
+            Boolean bArgsValid, bPerform;
+            strUserId = Master.UserId;
+            strCamperUserId = Master.CamperUserId;
+            strComments = txtComments.Text.Trim();
+            strFJCID = hdnFJCIDStep2_2.Value;
+            strCamperAnswers = ConstructCamperAnswers();
+            CamperAppl.CamperAnswersServerValidation(strCamperAnswers, strComments, strFJCID, strUserId, (Convert.ToInt32(Redirection_Logic.PageNames.Step2_2)).ToString(), strCamperUserId, out bArgsValid, out bPerform);
+            args.IsValid = bArgsValid;
+            bPerformUpdate = bPerform;
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+    }
+
+
+}
---
+++ CIPMSWeb/Enrollment/Madison/Summary.aspx
@@ -0,0 +1,66 @@
+<%@ Page Language="C#" MasterPageFile="~/Common.master" AutoEventWireup="true" CodeFile="Summary.aspx.cs" Inherits="Enrollment_Chi_Summary" %>
+
+<asp:Content ID="National_summary" ContentPlaceHolderID="Content" runat="Server">
+    <table width="100%" cellpadding="5" cellspacing="0" class="infotext3" style="text-align:justify">
+        <tr>
+            <td>
+                <img src="logo.jpg" />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <p>
+                    Great news!  The Jewish Federation of Madison (JFM) is thrilled to announce that we have partnered with the Foundation for Jewish Camp to bring the One Happy Camper program to Madison.  If you have a camper in grades 2nd-12th they may be eligible for a grant to attend Jewish overnight camp for the first time.
+                </p>
+                <p>
+                    To determine if you are eligible for the One Happy Camper grant please read below.  If you believe that your camper meets the criteria please proceed with the application by clicking the next button below.
+                </p>
+                <p>
+                    The JFM One Happy Camper Program provides grants to encourage children to attend overnight Jewish camp for the first-time.  It is not a scholarship fund and is not need-based.  Our goal is to engage families who are considering sending their children to Jewish overnight camp and, in effect, to give them up to $1,000 off their camp fees to try a Jewish one.
+                </p>
+				<p>
+				    The following outlines the eligibility criteria for this program:
+                    <ul style="font-weight: bold">
+                        <li>$1,000 grants awarded to first-time campers attending camp for 19 or more consecutive days.</li>
+                        <li>$700 grants awarded to first-time campers attending camp for 12 or more consecutive days</li>
+                        <li>If camper attended camp in the summer of 2014 for 12-18 days as a first time camper s/he is still eligible for the grant if attending camp in 2015 for 19 or more consecutive days.</li>
+                        <li>First time camper must be entering grades 2-12 (after camp).</li>
+                        <li>Attending one of the 150+ non-profit, Jewish, overnight camps listed on the Foundation for Jewish Camp’s website (<a href="http://www.OneHappyCamper.org/FindaCamp" target="_blank">www.OneHappyCamper.org/FindaCamp</a>).</li>
+                    </ul>
+				</p> 
+                <p>
+                    <strong>Note:</strong> This program is an outreach initiative for children who are not currently receiving an immersive, daily Jewish experience. As such, children who attend Jewish day school or Yeshiva are not eligible for program. If your child is not eligible and/or are interested in learning about financial-needs based grants or other camper funding opportunities please visit 
+                    <a href="http://JewishCamp.org/Scholarships" target="_blank">JewishCamp.org/Scholarships</a>, contact your camp, or the contact person listed at the bottom of this page.
+                </p>  
+                <p>
+                    If you need additional assistance, please call Ellen Weismer (608)278-1808.
+                </p>            
+            </td>
+        </tr>
+    </table>
+    <asp:Panel ID="Panel1" runat="server">
+        <table width="100%" cellpadding="1" cellspacing="0" border="0">
+            <tr>
+                <td valign="top" style="width: 5%">
+                    <asp:Label ID="Label12" runat="server" Text="" CssClass="QuestionText"></asp:Label></td>
+                <td valign="top">
+                    <br />
+                    <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                        <tr>
+                            <td align="left">
+                                <asp:Button Visible="false" ID="btnReturnAdmin" runat="server" Text="Exit To Camper Summary" CssClass="submitbtn1" OnClick="btnReturnAdmin_Click" /></td>
+                            <td>
+                                <asp:Button ID="btnPrevious" CausesValidation="false" runat="server" Text=" << Previous" CssClass="submitbtn" OnClick="btnPrevious_Click" /></td>
+                            <td align="center">
+                                <asp:Button ID="btnSaveandExit" CausesValidation="false" runat="server" Text="Save & Continue Later" CssClass="submitbtn1" />
+                            </td>
+                            <td align="right">
+                                <asp:Button ID="btnNext" CausesValidation="false" runat="server" Text="Next >> " CssClass="submitbtn" OnClick="btnNext_Click" /></td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </asp:Panel>
+</asp:Content>
+
