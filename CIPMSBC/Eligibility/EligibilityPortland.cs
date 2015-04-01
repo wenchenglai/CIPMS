c263833f265ee430188f21ba3d86e557b957cea4
wenlai <wenchenglai@gmail.com>
2015-04-01 16:16:27 -0400

    disable portland day school

--- CIPMSBC/Eligibility/EligibilityPortland.cs
+++ CIPMSBC/Eligibility/EligibilityPortland.cs
@@ -100,7 +100,7 @@ namespace CIPMSBC.Eligibility
                         if (specialCode == "PJGTC2015")
                             iStatusValue = (int)StatusInfo.PendingPJLottery;
                         else
-                            iStatusValue = (int)StatusInfo.SystemEligible;
+                            iStatusValue = (int)StatusInfo.SystemInEligible;
                     }
                     else
                     {
