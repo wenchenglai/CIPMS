1e65d2b86d5133b32ae5ee579513e7c37137de37
wenchenglai <wenchenglai@gmail.com>
2016-04-03 21:41:52 -0400

    fixed Washington Security

--- CIPMSWeb/Enrollment/Washington/Step2_2.aspx.cs
+++ CIPMSWeb/Enrollment/Washington/Step2_2.aspx.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Data;
 using System.Configuration;
+using System.Linq;
 using System.Web;
 using System.Web.UI.WebControls;
 using CIPMSBC;
@@ -18,6 +19,9 @@ public partial class Step2_Washington_2 : System.Web.UI.Page
 
     protected void Page_Init(object sender, EventArgs e)
     {
+        if (!ConfigurationManager.AppSettings["OpenFederations"].Split(',').Any(id => id == ((int)FederationEnum.WashingtonDC).ToString()))
+            Response.Redirect("~/NLIntermediate.aspx");
+
         btnNext.Click += new EventHandler(btnNext_Click);
         btnPrevious.Click += new EventHandler(btnPrevious_Click);
         btnSaveandExit.Click += new EventHandler(btnSaveandExit_Click);
--- CIPMSWeb/Enrollment/Washington/Step2_3.aspx.cs
+++ CIPMSWeb/Enrollment/Washington/Step2_3.aspx.cs
@@ -1,13 +1,9 @@
 using System;
 using System.Data;
 using System.Configuration;
-using System.Collections;
-using System.Web;
-using System.Web.Security;
+using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 using CIPMSBC;
 using CIPMSBC.Eligibility;
 
@@ -21,6 +17,9 @@ public partial class Step2_Washington_3 : Page
     private RadioButton RadioButtonQ7Option2;
     protected void Page_Init(object sender, EventArgs e)
     {
+        if (!ConfigurationManager.AppSettings["OpenFederations"].Split(',').Any(id => id == ((int)FederationEnum.WashingtonDC).ToString()))
+            Response.Redirect("~/NLIntermediate.aspx");
+
         btnChkEligibility.Click += new EventHandler(btnChkEligibility_Click);
         btnPrevious.Click += new EventHandler(btnPrevious_Click);
         //ddlState.SelectedIndexChanged += new EventHandler(ddlState_SelectedIndexChanged);
